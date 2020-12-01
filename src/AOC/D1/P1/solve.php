c5d73ede661d99c3b49be15e95573ee99f858645
Michael Leach <michael@mgldev.co.uk>
2020-12-01 15:42:47 +0000

    Completes Day 1

--- src/AOC/D1/P1/solve.php
+++ src/AOC/D1/P1/solve.php
@@ -1,1 +1,13 @@
 <?php
+
+namespace AOC\D1\P1;
+
+use AOC\Helper\InputReader;
+
+require_once __DIR__ . '/../../../../vendor/autoload.php';
+
+$numbers = InputReader::fileToLines(__DIR__ . '/input.txt');
+$matches = array_values(array_intersect(array_map(fn ($number) => 2020 - $number, $numbers), $numbers));
+$answer = array_product($matches);
+
+die('Part 1 is ' . $answer . "\n");
--- src/AOC/Helper/InputReader.php
+++ src/AOC/Helper/InputReader.php
@@ -1,7 +1,21 @@
 <?php
 
+namespace AOC\Helper;
 
-class InputReader
+/**
+ * Class InputReader
+ *
+ * @package AOC\Helper
+ */
+final class InputReader
 {
-
+    /**
+     * @param string $filename
+     *
+     * @return array
+     */
+    public static function fileToLines(string $filename): array
+    {
+        return array_filter(explode(PHP_EOL, file_get_contents($filename)));
+    }
 }
